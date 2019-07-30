61aff206ef107ca28373d8cb78544edd9746fbda
Joseph Rehfuss <josephrehfuss@D-172-22-194-158.pce.washington.edu>
2019-07-30 14:17:39 -0700

    Updates

---
+++ precache-manifest.2271936ddcab7b21cb1a5232655cb101.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5521fd6c14e91fe685beb413dc5193d",
+    "url": "/clickygamedeploydemo/index.html"
+  },
+  {
+    "revision": "1843af10d041397113e0",
+    "url": "/clickygamedeploydemo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/clickygamedeploydemo/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1843af10d041397113e0",
+    "url": "/clickygamedeploydemo/static/js/main.cae911d3.chunk.js"
+  },
+  {
+    "revision": "1856d5a0a79018c18a83",
+    "url": "/clickygamedeploydemo/static/js/runtime~main.0c77d986.js"
+  }
+]);
